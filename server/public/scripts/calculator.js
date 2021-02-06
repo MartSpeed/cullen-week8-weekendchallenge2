d86cece739689cdb994cf65ea4036931fbd7af4a
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-06 16:38:53 -0600

    added calculator function for buttons

--- server/public/scripts/calculator.js
+++ server/public/scripts/calculator.js
@@ -1,5 +1,17 @@
+// document call
 $(document).ready(onReady);
+
+// preset variable for functionality test
 let spiderMan = true;
+
+// jQuery active functions and rules
 function onReady() {
   console.log(`JQuery: ${spiderMan}`);
+
+  // call the math function based on button pressed
+  clientCalculator();
+}
+
+function clientCalculator() {
+  console.log(`inside the clientCalculator`, spiderMan);
 }
